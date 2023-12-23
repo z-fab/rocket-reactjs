4b606ebba61b10eb653b34be03aec498067e7b7e
Fabricio Zillig <fabricio.zillig@gmail.com>
2023-12-23 12:25:03 -0300

    Alterando HTML e CSS App

--- 01-desafio-todo/src/App.tsx
+++ 01-desafio-todo/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Header</h1>
+
+      <section className={styles.content}>
+        <div className={styles.taskInfoContainer}>
+          <h2>Tasks</h2>
+        </div>
+
+        <div className={styles.tasksList}>
+          <h2>List</h2>
+
+        </div>
+
+      </section>
+    </main>
   )
 }
-
-export default App
